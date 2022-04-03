503fe8bbc560ce27937cb127393b044e956ba2fe
saitotm <tomokazu.ml.tech@gmail.com>
2022-04-03 12:08:35 +0900

    add hlt in loop

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 
 use core::intrinsics;
 use core::panic::PanicInfo;
+use x86_64::instructions::hlt;
 
 #[panic_handler]
 #[no_mangle]
@@ -21,5 +22,7 @@ pub extern "C" fn _start() -> ! {
             .write_volatile(0x30);
     }
 
-    loop {}
+    loop {
+        hlt();
+    }
 }
